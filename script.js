d3ff2a23733f61b654505efe3689e7f151c47ee2
M.Hoshino <47610199+maimaistr@users.noreply.github.com>
2023-07-17 10:02:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 $(function(){
 	var loader = $('.loader-wrap');
 
+  //スムーススクロール
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+
    //MENU
    $(".menu-trigger").click(function () {
     $(this).toggleClass("active");
