ee23fb10bd1eb06d80386d90e5398d350a718b7b
M.Hoshino <47610199+maimaihsn@users.noreply.github.com>
2023-07-19 17:28:28 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -31,33 +31,12 @@ $(function(){
 		loader.fadeOut();
 	},3000);
 
-  //スクロール、クリックイベント
+  //スクロール
   $('top-wrapper a').click(function(){
    var id = $(this).attr('href');
    var position = $(id).offset().top;
     $('html,body').animate({
      'scrollTop': position
-   }, 500);
-  });
-
-  var topBtn = $('.go-to-top');
-      //ボタンを非表示にする
-      topBtn.hide();
-      //スクロールしてページトップから100に達したらボタンを表示
-      $(window).scroll(function () {
-          if ($(this).scrollTop() > 100) {
-  　　　　　　　//フェードインで表示
-              topBtn.fadeIn();
-          } else {
-  　　　　　　　//フェードアウトで非表示
-              topBtn.fadeOut();
-          }
-      });
-
-//トップに戻る
-  $('.go-to-top').click(function(){
-    $('html,body').animate({
-     'scrollTop': 0
    }, 500);
   });
 
