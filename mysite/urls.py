5fd5f9e5f71885cb2ed968740a89684be802b744
b-hoshino <maikohsn627@gmail.com>
2025-05-07 00:01:51 +0900

    Show index.html on top page

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
+    path('', views.home),
 ]
 
 # メディアファイルのURLルーティング
