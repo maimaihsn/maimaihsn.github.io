47e08ea8faf954e80134c54bc86e1b9c4245b496
b-hoshino <maikohsn627@gmail.com>
2025-05-07 00:22:51 +0900

    Fix: import views in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from mysite import views  # ← 必須！
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
